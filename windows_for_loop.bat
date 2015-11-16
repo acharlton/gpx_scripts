c87d99426003b1a8925907a4ed227277575bab9c
Anthony Charlton <anthony.charlton@ctbto.org>
2015-11-16 14:53:53 +0100

    reformatted line

--- windows_for_loop.bat
+++ windows_for_loop.bat
@@ -1,5 +1,4 @@
 # windows version
 
 
-C:\Users\charlton\Desktop\Archive>for %f in (C:\Users\charlton\Desktop\Archive\*.gpx) do C:\OSGeo4W64\bin\ogr2ogr -append -f
-"PostgreSQL" PG:"host=localhost dbname=gpx user=postgres password=postgres" "%f"
+C:\Users\charlton\Desktop\Archive>for %f in (C:\Users\charlton\Desktop\Archive\*.gpx) do C:\OSGeo4W64\bin\ogr2ogr -append -f "PostgreSQL" PG:"host=localhost dbname=gpx user=postgres password=postgres" "%f"
